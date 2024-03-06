7324c6065b4a2e474c22e4db18fa1f18f5df3be2
AxLamelas <alamelas@ua.pt>
2024-03-06 12:11:26 +0000

    Simplified to only core

--- qt_designer/convert_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for uifile in *.ui
-  do
-  echo $uifile
-  pyname=$(basename -s .ui $uifile ).py
-  echo $pyname
-  pyuic5 $uifile -o $pyname
-done
-
--- specific_tools/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-PyFDD package
-"""
-
-from .ecsli_tools import load_tpxquad_mask
-from .ieap_tools import make_array_from_scatterdata
